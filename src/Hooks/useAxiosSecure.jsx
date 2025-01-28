ef0a8dace16949f127d9c2c90e46e53e6417d1ea
hasibprangon <hasibulhossainp09@gmail.com>
2025-01-28 21:03:39 +0600

    vercel domain added

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://red-drop-heroes-server.vercel.app'
 }) 
 
 const useAxiosSecure = () => {
